e064a0d7bebeec708ccd9a934bb57ed7775edd33
dos257 <149811921+dos257@users.noreply.github.com>
2024-09-09 23:04:42 +0200

    setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
 
 try:
     readme = open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read()
